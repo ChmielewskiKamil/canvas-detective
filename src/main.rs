bfaa442d0a07a6ac9a7a0c289360b2d3306db593
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-07-12 22:46:42 +0200

    ✅ Test single issue parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test_parsing {
+    use super::*;
+    #[test]
+    fn it_should_parse_single_issue() {
+        let path_to_markdown_file: &str = "tests/test_data/001.md";
+
+        let parsing_result: Issue = parse_markdown_file(&path_to_markdown_file);
+
+        let expected_result = Issue {
+            issue_number: 001,
+            watson: "John Doe".to_string(),
+            severity: "medium".to_string(),
+            title: "This is a medium severity bug".to_string(),
+        };
+
+        assert_eq!(parsing_result, expected_result);
+    }
+}
